ab33be6fe1944da0e2ee32410f5bd728602eb206
mateus <mateus.silvafreitas27@gmail.com>
2025-10-17 23:42:19 -0300

    welcome screen

--- app/src/main/java/com/example/moviestreaming/presenter/splash/SplashScreen.kt
+++ app/src/main/java/com/example/moviestreaming/presenter/splash/SplashScreen.kt
@@ -1,10 +1,7 @@
 package com.example.moviestreaming.presenter.splash
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
