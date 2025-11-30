78da1f6c0c4322ccc4336f2248cc3ef9e8d7b1f4
mateus <mateus.silvafreitas27@gmail.com>
2025-11-30 12:42:00 -0300

    singup screen

---
+++ app/src/main/java/com/example/moviestreaming/presenter/screens/authentication/signup/screen/SignupScreen.kt
@@ -0,0 +1,223 @@
+package com.example.moviestreaming.presenter.screens.authentication.signup.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.moviestreaming.R
+import com.example.moviestreaming.presenter.components.button.PrimaryButton
+import com.example.moviestreaming.presenter.components.button.SocialButton
+import com.example.moviestreaming.presenter.components.divider.HorizontalDividerWithText
+import com.example.moviestreaming.presenter.components.textField.TextFieldUI
+import com.example.moviestreaming.presenter.components.topAppBar.TopAppBarUI
+import com.example.moviestreaming.presenter.theme.MovieStreamingTheme
+import com.example.moviestreaming.presenter.theme.UrbanistFamily
+
+@Composable
+fun SignupScreen(
+    onBackPressed: () -> Unit
+) {
+    SignupContent(
+        onBackPressed = onBackPressed
+    )
+}
+
+@Composable
+fun SignupContent(
+    onBackPressed: () -> Unit = {}
+) {
+    Scaffold(
+        topBar = {
+            TopAppBarUI(
+                onclick = onBackPressed
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MovieStreamingTheme.colorScheme.backgroundColor)
+                    .verticalScroll(rememberScrollState())
+                    .padding(paddingValues)
+                    .padding(horizontal = 24.dp, vertical = 48.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(120.dp)
+
+                )
+
+                Spacer(modifier = Modifier.height(48.dp))
+
+                Text(
+                    text = stringResource(id = R.string.label_title_singup_screen),
+                    style = TextStyle(
+                        fontSize = 32.sp,
+                        lineHeight = 38.4.sp,
+                        fontFamily = UrbanistFamily,
+                        fontWeight = FontWeight.Bold,
+                        color = MovieStreamingTheme.colorScheme.textColor,
+                        textAlign = TextAlign.Center
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(48.dp))
+
+                TextFieldUI(
+                    modifier = Modifier,
+                    value = "",
+                    placeholder = stringResource(id = R.string.label_input_email_singup_screen),
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_email),
+                            contentDescription = null
+                        )
+                    },
+                    onValueChange = {},
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                TextFieldUI(
+                    modifier = Modifier,
+                    value = "",
+                    placeholder = stringResource(id = R.string.label_input_password_singup_screen),
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_password),
+                            contentDescription = null
+                        )
+                    },
+                    tralingIcon = {
+                        IconButton(
+                            onClick = {},
+                            content = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_show),
+                                    contentDescription = null
+                                )
+                            }
+                        )
+                    },
+                    onValueChange = {},
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                PrimaryButton(
+                    text = stringResource(id = R.string.label_button_singup_screen),
+                    enabled = true,
+                    isLoading = false,
+                    onclick = {}
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                HorizontalDividerWithText(text = stringResource(id = R.string.label_or_singup_screen))
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    SocialButton(
+                        icon = painterResource(id = R.drawable.ic_google),
+                        onclick = {}
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    SocialButton(
+                        icon = painterResource(id = R.drawable.ic_facebook),
+                        onclick = {}
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    SocialButton(
+                        icon = painterResource(id = R.drawable.ic_github),
+                        onclick = {}
+                    )
+                }
+                
+                Spacer(modifier = Modifier.height(48.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.label_sing_in_account_singup_screen),
+                        style = TextStyle(
+                            lineHeight = 19.6.sp,
+                            fontFamily = UrbanistFamily,
+                            color = MovieStreamingTheme.colorScheme.textColor,
+                            textAlign = TextAlign.Right,
+                            letterSpacing = 0.2.sp
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.label_sing_in_singup_screen),
+                        style = TextStyle(
+                            lineHeight = 19.6.sp,
+                            fontFamily = UrbanistFamily,
+                            fontWeight = FontWeight.SemiBold,
+                            color = MovieStreamingTheme.colorScheme.defaultColor,
+                            textAlign = TextAlign.Right,
+                            letterSpacing = 0.2.sp
+                        )
+                    )
+                }
+            }
+        }
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun SignupScreenPreview() {
+    MovieStreamingTheme{
+        SignupScreen(
+            onBackPressed = {}
+        )
+    }
+}
